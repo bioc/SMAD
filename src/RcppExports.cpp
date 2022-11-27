da3da683ec6381457448113600e2ad3c4fa495c7
Qingzhou Zhang <zhang40q@MBP-zqz.local>
2022-11-27 07:17:49 -0500

    Minor

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // GetDICE
 NumericMatrix GetDICE(NumericMatrix mat);
 RcppExport SEXP _SMAD_GetDICE(SEXP matSEXP) {
